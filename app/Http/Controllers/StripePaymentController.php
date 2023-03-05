f0c537f4716e012c3f0e2f6d8f16e3c89c5fec29
workload2022 <mohosin.csm@gmail.com>
2023-03-05 18:48:13 +0600

    Update API Code With Latest

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -26,16 +26,69 @@ class StripePaymentController extends Controller
     public function stripePost(Request $request)
     {
         Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-    
-        Stripe\Charge::create ([
-                "amount" => 100 * 100,
-                "currency" => "usd",
-                "source" => $request->stripeToken,
-                "description" => "Test payment from itsolutionstuff.com." 
-        ]);
-      
-        Session::flash('success', 'Payment successful!');
-              
-        return back();
+
+        $customer = Stripe\Customer::create(array(
+
+            "address" => [
+
+                    "line1" => "Virani Chowk",
+
+                    "postal_code" => "360001",
+
+                    "city" => "Rajkot",
+
+                    "state" => "GJ",
+
+                    "country" => "IN",
+
+                ],
+
+            "email" => "hossain@gmail.com",
+
+            "name" => "Hossain Jamal",
+
+            "source" => $request->stripeToken
+
+         ));
+  
+        Stripe\PaymentIntent::create([
+            'amount' => 1000 * 10,
+            'currency' => 'inr',
+            'payment_method_types' => ['card'],
+            'metadata' => [
+              'order_id' => '12345',
+            ],
+            "customer" => $customer->id,
+
+            "description" => "Test payment from itsolutionstuff.com.",
+
+            "shipping" => [
+
+              "name" => "Jon Due",
+
+              "address" => [
+
+                "line1" => "510 Townsend St",
+
+                "postal_code" => "98140",
+
+                "city" => "Fardabad",
+
+                "state" => "CA",
+
+                "country" => "US",
+
+              ],
+
+            ]
+          ]);
+
+  
+
+    Session::flash('success', 'Payment successful!');
+
+           
+
+    return back();
     }
 }
